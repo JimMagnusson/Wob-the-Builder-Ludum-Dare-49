4b286b036bc1e5fc596eb8964adcf7e27090a3bd
Jim Magnusson <jim.magnusson97@gmail.com>
2021-10-02 01:23:27 +0200

    Started working on block movement

---
+++ Assets/BlockMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMover : MonoBehaviour
+{
+    [SerializeField] private Transform currentBlock;
+    private Rigidbody currentBlockRB;
+
+    [SerializeField] private float moveSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBlockRB = currentBlock.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentBlock == null) { return; }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
