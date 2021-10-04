f0d9bdcbc17d1ac227d89104ba84636b1ec05363
Jim Magnusson <jim.magnusson97@gmail.com>
2021-10-04 20:53:46 +0200

    Fixed end screen, fonts, added musc and mute button.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	public static MusicPlayer Instance = null;
+
+	private AudioSource audioSource;
+	private bool toggled = false;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void MuteToggle()
+    {
+		if(!toggled)
+        {
+			toggled = true;
+			audioSource.mute = true;
+		}
+		else
+        {
+			toggled = false;
+			audioSource.mute = false;
+		}
+	}
+}
---
+++ Assets/SwitchSpritesOnClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class SwitchSpritesOnClick : MonoBehaviour,IPointerClickHandler
+{
+    [SerializeField] Sprite normalSprite = null;
+    [SerializeField] Sprite clickedSprite = null;
+
+    private bool clicked = false;
+    private Image image = null;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        if (!clicked)
+        {
+            image.sprite = clickedSprite;
+            clicked = true;
+        }
+        else
+        {
+            image.sprite = normalSprite;
+            clicked = false;
+        }
+    }
+}
