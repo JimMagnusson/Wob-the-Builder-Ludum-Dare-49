5eaa5134c19a9b3df83a40ddcea66e96795b16df
Jim Magnusson <jim.magnusson97@gmail.com>
2021-10-04 19:49:57 +0200

    Main menu fixed

---
+++ Assets/LevelSelectScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectScreen : MonoBehaviour
+{
+    public void ToggleActive()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
